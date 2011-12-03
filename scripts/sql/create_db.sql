1c6e50a1ac39c6bd371fca5126ece4d39b7fa4d7
Bennet Huber <bhuber@azavea.com>
2011-12-03 14:40:27 -0500

    Added Table create script, with simple insert statement for adding test data.

---
+++ scripts/sql/create_db.sql
@@ -0,0 +1,25 @@
+-- SQL for creating our SQLite table(s)
+
+-- Tried to add CHECK constraints, but they don't seem to work in sqlite :(
+CREATE TABLE ClimateGrid (
+    Row INT NOT NULL,
+    Col INT NOT NULL,
+    Precip FLOAT,
+    Min_T FLOAT,
+    Max_T FLOAT,
+    Seq INT NOT NULL,
+    Year INT NOT NULL,
+    Month INT NOT NULL,
+    Day INT NOT NULL);
+--CHECK (Row > 0 AND Row < 720)
+--CHECK (Col > 0 AND Col < 360)
+--CHECK (Year > 0)
+
+-- Create test data
+INSERT INTO ClimateGrid (Row, Col, Precip, Min_T, Max_T, Seq, Year, Month, Day)
+    VALUES (180, 180, 3.0, -5, 100, 180 * 720 + 180, 1998, 3, 5);
+
+-- Insert Data template
+--INSERT INTO ClimateGrid (Row, Col, Precip, Min_T, Max_T, Seq, Year, Month, Day)
+    --VALUES ();
+
