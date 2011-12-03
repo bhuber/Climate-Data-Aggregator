80564e0af9362211e5b1d84f80c24712d8e53200
Bennet Huber <bhuber@azavea.com>
2011-12-03 18:24:42 -0500

    Finished dbinterface.py so it actually works, refactored dataimport.py to use it.

--- py/dataimport.py
+++ py/dataimport.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, tarfile
-from pysqlite2 import dbapi2 as sqlite3
 import datetime
+from dbinterface import ClimateGridInterface
 
+#Usage:
+# p='../data/test'
+# python dataimport.py $p/pre.20061231.asc $p/tmin.20061231.asc $p/tmax.20061231.asc
 
 def import_file(filename1, filename2, filename3):
-	
 	print "Reading files...",
 	
 	# Split each file into items.
@@ -21,72 +23,13 @@ def import_file(filename1, filename2, filename3):
 	start = datetime.datetime(year, 1, 1)
 	now = datetime.datetime(year, month, day)
 	doy = (now-start).days
-	
-	#Docs for pysqlite: http://pysqlite.googlecode.com/svn/doc/sqlite3.html
 
-	create_statement = """
-		CREATE TABLE ClimateGrid (
-			Row INT NOT NULL,
-			Col INT NOT NULL,
-			Precip FLOAT,
-			Min_T FLOAT,
-			Max_T FLOAT,
-			Seq INT NOT NULL,
-			Year INT NOT NULL,
-			Month INT NOT NULL,
-			Day INT NOT NULL)"""
-	
-	insert_statement = """
-		INSERT INTO ClimateGrid (Row, Col, Precip, Min_T, Max_T, Seq, Year, Month, Day)
-			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
-	
-	
-	class Helper:
-		@staticmethod
-		def index_to_sequence(row, col):
-			"""Converts a (row, col) index to a sequence number
-			"""
-			return col * 720 + row
-	
-		@staticmethod
-		def offset_to_month_day(year, offset):
-			"""
-			Given a year and a number of days offset, returns a tuple
-			(month, day) for the date of first day of year
-			+ offset
-			"""
-			start = datetime.datetime(year, 1, 1)
-			offset = datetime.timedelta(offset)
-			end = start + offset
-			return (end.month, end.day)
-	
-	#connect can take a file path for a db file, or ':memory:' to create a db in RAM
-	conn = sqlite3.connect('./'+str(year)+str(month)+str(day)+".sql")
-	c = conn.cursor()
-	c.execute(create_statement)
-	
-	def insert_sql(fa, fb, fc, year, month, day, doy, c):
-		for i in xrange(len(fa)):
-			if -999 in (fa[i],fb[i],fc[i]):
-				continue
-			row, col = int(i/360),i%720
-			precip = None
-			min_temp = None
-			max_temp = None
-			precip = fa[i]
-			min_temp = fb[i]
-			max_temp = fc[i]
-			insert_data = (row, col, precip, min_temp, max_temp, i, year, month, day)
-			c.execute(insert_statement, insert_data)
-	
-	insert_sql(fa, fb, fc, year, month, day, doy, c)
+	cgi = ClimateGridInterface()
+	cgi.import_files(fa, fb, fc, year, month, day, doy)
 	
 	print "Success!"
 	
-	#print(c.execute("SELECT * FROM ClimateGrid WHERE Row = 80 AND Col = 135").fetchall())
-
-	conn.commit()
-	conn.close()
+	print(cgi.curs.execute("SELECT * FROM ClimateGrid WHERE Row = 80 AND Col = 135").fetchall())
 
 if __name__=="__main__":
 	if len(sys.argv)<4:
--- py/dbinterface.py
+++ py/dbinterface.py
@@ -4,64 +4,69 @@ from pysqlite2 import dbapi2 as sqlite3
 import datetime
 
 class ClimateGridInterface:
-    """
-    Defines some interface methods for our ClimateGrid table
-    Automatically connects to the db upon creation and creates the necessary tables
-    """
+	"""
+	Defines some interface methods for our ClimateGrid table
+	Automatically connects to the db upon creation and creates the necessary tables
+	"""
 
-    conn = None     #db connection
-    curs = None     #cursor for connection
+	conn = None	 #db connection
+	curs = None	 #cursor for connection
 
-    def __init__():
-        conn = sqlite3.connect("./crimegrid.sqlite")
-        c = conn.cursor()
-        #TODO: test for existence of our db, create only if necessary
-        c.execute(self._create_statement)
+	def __init__(self):
+	#Docs for pysqlite: http://pysqlite.googlecode.com/svn/doc/sqlite3.html
+		self.conn = sqlite3.connect("./testdb.sqlite")
+		self.curs = self.conn.cursor()
+		#TODO: test for existence of our db, create only if necessary
+		self.curs.execute(self._create_statement)
 
-    _nrows = 360
-    _ncols = 720
+	def __del__(self):
+		self.conn.commit()
+		self.conn.close()
 
-    _create_statement = """
-        CREATE TABLE ClimateGrid (
-            Row INT NOT NULL,
-            Col INT NOT NULL,
-            Precip FLOAT,
-            Min_T FLOAT,
-            Max_T FLOAT,
-            Seq INT NOT NULL,
-            Year INT NOT NULL,
-            Month INT NOT NULL,
-            Day INT NOT NULL)"""
+	_nrows = 360
+	_ncols = 720
 
-    _insert_statement = """
-        INSERT INTO ClimateGrid (Row, Col, Precip, Min_T, Max_T, Seq, Year, Month, Day)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+	_create_statement = """
+	CREATE TABLE ClimateGrid (
+		Row INT NOT NULL,
+		Col INT NOT NULL,
+		Precip FLOAT,
+		Min_T FLOAT,
+		Max_T FLOAT,
+		Seq INT NOT NULL,
+		Year INT NOT NULL,
+		Month INT NOT NULL,
+		Day INT NOT NULL)"""
 
-    def round_to_nearest_half(x):
-        return round(2 * x) / 2
+	_insert_statement = """
+		INSERT INTO ClimateGrid (Row, Col, Precip, Min_T, Max_T, Seq, Year, Month, Day)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 
-    def latlng_to_rowcol(lat, lng):
-        """
-        Given latitude and longitude returns (row, col) for grid 
-        or None if out of bounds
-        """
-        if lat < -90 or lat > 90 or lng < -180 or lng > 180:
-            return None
-        lat = self.round_to_nearest_half(lat)
-        lng = self.round_to_nearest_half(lng)
-        row = (lat + 90) * _nrows / 180
-        col = (lng + 180) * _ncols / 360
-        row = int(row) % _nrows
-        col = int(col) % _ncols
-        return (row, col)
+	def round_to_nearest_half(self, x):
+		return round(2 * x) / 2
 
-    def index_to_sequence(row, col):
-        """
-        Converts a (row, col) index to a sequence number
-        """
-        return col * 720 + row
+	def latlng_to_rowcol(self, lat, lng):
+		"""
+		Given latitude and longitude returns (row, col) for grid 
+		or None if out of bounds
+		"""
+		if lat < -90 or lat > 90 or lng < -180 or lng > 180:
+			return None
+		lat = self.round_to_nearest_half(lat)
+		lng = self.round_to_nearest_half(lng)
+		row = (lat + 90) * _nrows / 180
+		col = (lng + 180) * _ncols / 360
+		row = int(row) % _nrows
+		col = int(col) % _ncols
+		return (row, col)
 
-	def import_files(fa, fb, fc, year, month, day, doy, c):
+	def index_to_sequence(self, row, col):
+		"""
+		Converts a (row, col) index to a sequence number
+		"""
+		return col * 720 + row
+
+	def import_files(self, fa, fb, fc, year, month, day, doy):
 		for i in xrange(len(fa)):
 			if -999 in (fa[i],fb[i],fc[i]):
 				continue
@@ -73,37 +78,10 @@ class ClimateGridInterface:
 			min_temp = fb[i]
 			max_temp = fc[i]
 			insert_data = (row, col, precip, min_temp, max_temp, i, year, month, day)
-			self.insert_row(insert_statement, insert_data)
+			self.insert_row(insert_data)
 
-    def insert_row(data):
-       self.curs.execute(self._insert_statement, insert_data)
+	def insert_row(self, data):
+	   self.curs.execute(self._insert_statement, data)
 	
-        
-
-class Helper:
-    @staticmethod
-    def index_to_sequence(row, col):
-        """
-        Converts a (row, col) index to a sequence number
-        """
-        return col * 720 + row
-
-    @staticmethod
-    def rand_temp():
-        """Returns a random temperature 
-        """
-        return random.randint(-30, 110) + random.random();
-
-    @staticmethod
-    def offset_to_month_day(year, offset):
-        """
-        Given a year and a number of days offset, returns a tuple
-        (month, day) for the date of first day of year
-         + offset
-        """
-        start = datetime.datetime(year, 1, 1)
-        offset = datetime.timedelta(offset)
-        end = start + offset
-        return (end.month, end.day)
-
+	
 
