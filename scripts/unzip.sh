b4cdde86dd3e088bb0a42e145072c629366812ba
Bennet Huber <bhuber@azavea.com>
2011-12-04 11:34:25 -0500

    Fixed execall.sh, minor refactor to unzip.sh

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #Usage: execall.sh '/path/to/PRCP/' '/path/to/MAX Temp/' '/path/to/MIN TEMP/' startYear endYear
+#p="/foo/path/to/data"
+#./unzip.sh "${p}PRCP/" "${p}MAX TEMP/" "${p}MIN TEMP/" 2000 2006
 #Don't forget the trailing slashes!
 
 start="$4"
 end="$5"
 outputdir="../data/real"
 
-#if [ -z $6 ]; then
+#can't get this working for optional 6th argument, just change outputdir manually
+#if [ -n $6 ]; then
 	#outputdir="$6"
 	#echo $outputdir
 #fi
