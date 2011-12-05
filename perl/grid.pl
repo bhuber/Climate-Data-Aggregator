9f586fc60d5c6deed57f926be83d364b1fa83345
Amir Tahvildaran <amirdt22@gmail.com>
2011-12-05 18:25:04 -0500

    actually render the image instead of random

--- perl/grid.pl
+++ perl/grid.pl
@@ -15,11 +15,13 @@ my $num_rows = $opts{'r'} || 99999;
 my $num_cols = $opts{'c'} || 99999;
 my $steps = $opts{'s'} || 5;
 
+die "can't go above 255 steps" if ($steps > 255);
+
 my $row_count = 0;
 my $col_count = 0;
 
-my $min;
-my $max;
+my $min = "no";
+my $max = "no";
 
 
 my @data;
@@ -35,8 +37,11 @@ while(<DATA>) {
   my @elems = split(/\s+/, $line);
   foreach my $e (@elems) {
     next unless ($e);
-    if(undef($min)){ $min = $e; }
-    if(undef($max)){ $max = $e; }
+    if($e eq "-999.000"){ $e = 0; } #FLAG #TODO this needs to be set to null and transparent
+    if($min eq "no"){ $min = $e; }
+    if($max eq "no"){ $max = $e; }
+    if($e < $min && $e ne "-999.000") { $min = $e; }
+    if($e > $max) { $max = $e; }
     #print "$row_count,$col_count: $e\t";
     $col_count++;
     die "too many cols: $col_count $num_cols" if($col_count > $num_cols);
@@ -46,14 +51,17 @@ while(<DATA>) {
 }
 
 print "rows: $row_count cols: $col_count\n";
+print "min: $min max: $max\n";
 
 my $im = new GD::Image($col_count,$row_count);
 my @colors;
 
-my $white = $im->colorAllocate(255,255,255);
+my $color = 255;
+my $white = $im->colorAllocate($color,$color,$color);
 push(@colors, $white);
 for (my $i = 1; $i <= $steps; $i++){
-  my $color = int(255-255/$i);
+  $color -= int(255/$steps);
+  print "color: $color\n";
   push(@colors, $im->colorAllocate($color, $color, $color));
 }
 my $black = $im->colorAllocate(0,0,0);
@@ -62,11 +70,26 @@ open (PNG, ">out.png");
 binmode PNG;
 
 sub color_index { #choose a color shade based on max, min, 
+  #does this fall into the top $steps'th
+  my $low = shift;
+  my $high = shift;
+  my $datapoint = shift;
+  return 0 unless $datapoint;
+  if ($datapoint eq "-999.000") { return 0; }
+  #print "min:$min max:$max data:$datapoint\n";
+  my $range = $max-$min;
+  my $step = $range/$steps;
+  for (my $i = $steps; $i > 0; $i--) {
+    if ($datapoint > ($i * $step) ) {
+      return $i;
+    }
+  }
+  return 0;
 }
 
 for (my $r = 0; $r < $row_count; $r++) {
    for (my $c = 0; $c < $col_count; $c++) {
-     $im->setPixel($r, $c, $colors[int(rand(5))]);
+     $im->setPixel($r, $c, $colors[color_index($min,$max,$data[$r][$c])]);
    }
 }
 
